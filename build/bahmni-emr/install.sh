2c9c6196791241cac837f6f7bf32e6114cecb61b
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-11-26 14:44:19 +0530

    Bharath, Jaswanth | String services before dumping databases

--- build/bahmni-emr/install.sh
+++ build/bahmni-emr/install.sh
@@ -2,6 +2,7 @@
 
 restore_mysql_database(){
     #Optional Step
+    service mysqld start
     rm -rf mysql_backup.sql.gz mysql_backup.sql
     wget https://github.com/Bhamni/emr-functional-tests/blob/master/dbdump/mysql_backup.sql.gz?raw=true -O mysql_backup.sql.gz
     gzip -d mysql_backup.sql.gz
@@ -11,6 +12,7 @@ restore_mysql_database(){
 }
 
 restore_pgsql_db(){
+    service postgresql-9.2 start
     wget https://github.com/Bhamni/emr-functional-tests/blob/master/dbdump/pgsql_backup.sql.gz?raw=true -O pgsql_backup.sql.gz
     gzip -d pgsql_backup.sql.gz
     psql -Upostgres < pgsql_backup.sql >/dev/null
@@ -37,4 +39,3 @@ restore_mysql_database
 restore_pgsql_db
 install_bahmni
 config_services
-

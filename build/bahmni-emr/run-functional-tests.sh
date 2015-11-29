63fbff247a5ad21920eb76e77bc583fc88add26d
Venkata Jaswanth <jaswanth@thoughtworks.com>
2015-11-29 13:10:47 +0530

    Jaswanth | Changing permissions

--- build/bahmni-emr/run-functional-tests.sh
+++ build/bahmni-emr/run-functional-tests.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 yum localinstall -y /packages/*.rpm
+service openmrs restart
+service httpd restart
 cd emr-functional-tests
+mkdir screenshots || true
+chmod 777 screenshots || true
+mkdir spec-results || true
+chmod 777 spec-results || true
+sleep 30s
 sh scripts/run.sh
